3fed623dc94f5aa723b5d36f4ccc9b1987927028
Raymond Hammarling <raymond@hammarlings.se>
2016-04-12 15:30:41 +0200

    Add capturing and sending pic of vecka.nu on request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// The entry point of all to come...
+import webshot from 'webshot';
+import TelegramBot from 'node-telegram-bot-api';
+import stream from 'stream';
+
+const token = '201853302:AAFu5EcPgsba_U2b_QEd0ugZc_kXa5hDXyw';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/what week is it/i, (msg) => {
+    console.log(msg);
+
+    webshot('vecka.nu', 'out.png', (err) => {
+        bot.sendPhoto(msg.chat.id, 'out.png');
+    });
+});
